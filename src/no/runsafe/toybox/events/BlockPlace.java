3dd4e7377566e0e6849481e1255b9ed700ed8f3f
njs41 <njs42@hotmail.com>
2017-05-08 17:13:56 -0400

    Check for players trying to place infinite dispensers that shouldn't.
    In Minecraft 1.8+ players in creative are able to spawn in any item they want,
    including infinite dispensers. This makes sure that they won't be able
    to place them.

--- src/no/runsafe/toybox/events/BlockPlace.java
+++ src/no/runsafe/toybox/events/BlockPlace.java
@@ -1,9 +1,11 @@
 package no.runsafe.toybox.events;
 
 import no.runsafe.framework.api.block.IBlock;
+import no.runsafe.framework.api.block.IDispenser;
 import no.runsafe.framework.api.block.ISign;
 import no.runsafe.framework.api.event.block.IBlockPlace;
 import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
 
 public class BlockPlace implements IBlockPlace
 {
@@ -16,6 +18,23 @@ public class BlockPlace implements IBlockPlace
 			if (sign.getLine(0).equals("[Infinite]"))
 				if (!player.hasPermission("runsafe.toybox.infinitedispensers"))
 					return denyAction(player);
+		}
+		else if (block instanceof IDispenser)
+		{
+			RunsafeMeta item = player.getItemInHand();
+			try
+			{
+				if (item.getRawMeta() != null
+					&& item.hasDisplayName()
+					&& item.getDisplayName().equals("Infinite")
+					&& !player.hasPermission("runsafe.toybox.infinitedispensers")
+				)
+					return denyAction(player);
+			}
+			catch (NullPointerException e)
+			{
+				return true;
+			}
 		}
 		return true;
 	}
